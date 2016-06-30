aaca59e7d448c2dd64917a62e4c6a5e1d93a99de
Anastasia Tondera <a.tondera@gmail.com>
2016-06-30 18:30:02 +0200

    Remove lib .o files

--- app/src/main/obj/local/armeabi-v7a/objs/cpp_point_cloud_example/__/__/__/__/__/tango_gl/shaders.o.d
+++
@@ -1,8 +0,0 @@
-/home/anastasia/Projects/examples/tango-examples-c/cpp_point_cloud_example/app/src/main/obj/local/armeabi-v7a/objs/cpp_point_cloud_example/__/__/__/__/__/tango_gl/shaders.o: \
- /home/anastasia/Projects/examples/tango-examples-c/cpp_point_cloud_example/app/src/main/jni/../../../../../tango_gl/shaders.cc \
- /home/anastasia/Projects/examples/tango-examples-c/cpp_point_cloud_example/app/src/main/jni/../../../../../tango_gl/include/tango-gl/shaders.h \
- /home/anastasia/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/string
-
-/home/anastasia/Projects/examples/tango-examples-c/cpp_point_cloud_example/app/src/main/jni/../../../../../tango_gl/include/tango-gl/shaders.h:
-
-/home/anastasia/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/string:
